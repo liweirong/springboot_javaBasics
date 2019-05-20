2ee15121bf7f09f5b829b2530fec08cf61b98730
liweirong <2277839278@qq.com>
2019-05-20 22:09:34 +0800

    析构函数

---
+++ src/main/java/com/iris/springboot_all/work/BATJ/Basic.java
@@ -0,0 +1,18 @@
+package com.iris.springboot_all.work.BATJ;
+
+/**
+ * 每天一个小知识点
+ * @author iris
+ * @date 2019/5/20
+ */
+public class Basic {
+
+    // 1.析构函数
+    /**
+     * 析构函数(destructor) 与构造函数相反，当对象结束其生命周期，如对象所在的函数已调用完毕时，系统自动执行析构函数。
+     * 在java中，我们一般用不到它，因为 java 有自动内存回收机制，无须程序员手动释放。java 对象析构时会调用 finalize() 方法。
+     * 那么finalize()究竟是做什么的呢？它最主要的用途是回收特殊渠道申请的内存。Java程序有垃圾回收器，所以一般情况下内存问题不用程序员操心。
+     * 但有一种JNI(Java Native Interface)调用non-Java程序（C或C++），finalize()的工作就是回收这部分的内存。
+     */
+    //
+}
