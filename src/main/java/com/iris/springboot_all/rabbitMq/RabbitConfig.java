80650772c39cae379a882617529481422bb909d3
liweirong <2277839278@qq.com>
2019-09-17 23:17:36 +0800

    静态内部类

--- src/main/java/com/iris/springboot_all/rabbitMq/RabbitConfig.java
+++ src/main/java/com/iris/springboot_all/rabbitMq/RabbitConfig.java
@@ -1,18 +1,18 @@
-package com.iris.springboot_all.rabbitMq;
-
-import org.springframework.amqp.core.Queue;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * @author iris
- * @date 2019/8/20
- */
-@Configuration
-public class RabbitConfig {
-    @Bean
-    public Queue helloQueue() {
-        return new Queue("hello");
-    }
-}
-
+//package com.iris.springboot_all.rabbitMq;
+//
+//import org.springframework.amqp.core.Queue;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @author iris
+// * @date 2019/8/20
+// */
+//@Configuration
+//public class RabbitConfig {
+//    @Bean
+//    public Queue helloQueue() {
+//        return new Queue("hello");
+//    }
+//}
+//
