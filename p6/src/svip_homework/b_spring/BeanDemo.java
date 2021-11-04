0967e7a434cd65ecaeb65d12d4e0e853d35ebac6
liweirong <2277839278@qq.com>
2021-11-04 20:19:12 +0800

    13 springboot

---
+++ p6/src/svip_homework/b_spring/BeanDemo.java
@@ -0,0 +1,116 @@
+package svip_homework.b_spring;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.core.ResolvableType;
+
+/**
+ * @author iris
+ * @date 2021/11/4
+ */
+public class BeanDemo {
+    public static void main(String[] args) throws Exception {
+        Object object = new FactoryBeanDemo().getObject();
+        Object object2 = new BeanFactoryDemo().getBean("aa");
+        System.out.println(object);
+    }
+}
+
+class FactoryBeanDemo implements FactoryBean {
+    @Override
+    public Object getObject() throws Exception {
+        return "lwr FactoryBean demo";
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return String.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return FactoryBean.super.isSingleton();
+    }
+}
+
+class BeanFactoryDemo implements BeanFactory{
+
+
+    @Override
+    public Object getBean(String s) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public <T> T getBean(String s, Class<T> aClass) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public Object getBean(String s, Object... objects) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public <T> T getBean(Class<T> aClass) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public <T> T getBean(Class<T> aClass, Object... objects) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public <T> ObjectProvider<T> getBeanProvider(Class<T> aClass) {
+        return null;
+    }
+
+    @Override
+    public <T> ObjectProvider<T> getBeanProvider(ResolvableType resolvableType) {
+        return null;
+    }
+
+    @Override
+    public boolean containsBean(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean isSingleton(String s) throws NoSuchBeanDefinitionException {
+        return false;
+    }
+
+    @Override
+    public boolean isPrototype(String s) throws NoSuchBeanDefinitionException {
+        return false;
+    }
+
+    @Override
+    public boolean isTypeMatch(String s, ResolvableType resolvableType) throws NoSuchBeanDefinitionException {
+        return false;
+    }
+
+    @Override
+    public boolean isTypeMatch(String s, Class<?> aClass) throws NoSuchBeanDefinitionException {
+        return false;
+    }
+
+    @Override
+    public Class<?> getType(String s) throws NoSuchBeanDefinitionException {
+        return null;
+    }
+
+    @Override
+    public Class<?> getType(String s, boolean b) throws NoSuchBeanDefinitionException {
+        return null;
+    }
+
+    @Override
+    public String[] getAliases(String s) {
+        return new String[0];
+    }
+}
